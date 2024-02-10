b36dcbb8ee37abca3429a58bceca669bf975b28a
Yash Srivastava <85068689+yash-srivastava19@users.noreply.github.com>
2024-02-11 00:39:44 +0530

    Create agent_manager.py
    added dummy code.

---
+++ agent_manager.py
@@ -0,0 +1,6 @@
+class AgentManager:
+    def __init__(self, agents):
+        self.agents = agents
+
+    def process_interaction(self, query):
+        # ... (implement interaction logic based on query type and context)
